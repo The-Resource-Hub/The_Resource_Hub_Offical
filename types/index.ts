3ede5a6a8cf0e3ed0c01ecec19ab2a8f64b0ff9b
preet4241 <152093434+preet4241@users.noreply.github.com>
2026-01-02 12:21:49 +0530

    Add Product and FilterOption interfaces

---
+++ types/index.ts
@@ -0,0 +1,15 @@
+
+
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  category: string;
+  image: string;
+  rating: number;
+}
+
+export interface FilterOption {
+  label: string;
+  value: string;
+}
